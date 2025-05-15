36ce4b45ece344c3fdab9c3f8e9cf6a04f792dd5
Marc N. <git@marcnitzsche.de>
2025-05-15 11:26:00 +0200

    Add styles for HabitEntries

--- app/helpers/habits_helper.rb
+++ app/helpers/habits_helper.rb
@@ -1,2 +1,12 @@
 module HabitsHelper
+  STATUS_ICONS = {
+    completed: ["check", "green"],
+    failed: ["x-mark", "red"],
+    skipped: ["forward", "yellow"],
+    pending: [nil, "slate"]
+  }.freeze
+
+  def entry_status_icon_and_color(entry)
+    STATUS_ICONS[entry.status.to_sym] || STATUS_ICONS[:pending]
+  end
 end
