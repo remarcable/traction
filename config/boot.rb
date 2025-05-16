2da353ebf639d9edc8d70c567fe620e004ec8b41
Marc N. <git@marcnitzsche.de>
2025-05-16 15:11:47 +0200

    Remove manual dotenv initialization in boot.rb

--- config/boot.rb
+++ config/boot.rb
@@ -1,14 +1,4 @@
 ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 
-require 'dotenv'
-env_file = case ENV['RAILS_ENV'] || ENV['RACK_ENV']
-            when 'production'
-                '.env.production'
-            else
-                '.env'
-            end
-
-Dotenv.load(env_file) if File.exist?(env_file)
-
 require "bundler/setup" # Set up gems listed in the Gemfile.
 require "bootsnap/setup" # Speed up boot time by caching expensive operations.
